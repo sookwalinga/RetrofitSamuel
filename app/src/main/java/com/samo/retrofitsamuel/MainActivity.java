6a9fcb915852f80cbc37af44066ab8c81243608a
Samuel Okwalinga <sam.ictdev@gmail.com>
2021-03-21 16:50:10 +0300

    Update MainActivity

--- app/src/main/java/com/samo/retrofitsamuel/MainActivity.java
+++ app/src/main/java/com/samo/retrofitsamuel/MainActivity.java
@@ -1,18 +1,30 @@
 package com.samo.retrofitsamuel;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.samo.retrofitsamuel.adapter.PostListAdapter;
+import com.samo.retrofitsamuel.model.PostModel;
 import com.samo.retrofitsamuel.viewmodel.PostListViewModel;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    //Intance of List.
+    private List<PostModel> postModelList;
+    private PostListAdapter adapter;
+    private PostListViewModel viewModel;
 
     RecyclerView recyclerView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +33,32 @@ public class MainActivity extends AppCompatActivity {
 
 //        Initializing the recyclerview.
         recyclerView = findViewById(R.id.recview);
-
+        textView = findViewById(R.id.noDataView);
 //        Set the LinearLayoutManager to the recyclerview. I am using the GridLayoutManager in this case.
-        LinearLayoutManager linearLayoutManager = new GridLayoutManager(this, 2);
+        LinearLayoutManager linearLayoutManager = new GridLayoutManager(this, 4);
         recyclerView.setLayoutManager(linearLayoutManager);
 //        Instantiating the Adapter class.
-        PostListAdapter postListAdapter = new PostListAdapter();
-        recyclerView.setAdapter(postListAdapter);
+        PostListAdapter postListAdapter = new PostListAdapter(this, postModelList);
+        recyclerView.setAdapter(adapter);
+
+        viewModel = ViewModelProviders.of(this).get(PostListViewModel.class);
+//        listening to the live data.
+        viewModel.getPostListObserver().observe(this, new Observer<List<PostModel>>() {
+            //            Onsuccess, display the data.
+            @Override
+            public void onChanged(List<PostModel> postModels) {
+                if (postModels != null) {
+                    postModelList = postModels;
+                    postListAdapter.setPostList(postModels);
+//                    Set visibility for the textView for no data to gone when data is fetched successfully.
+                    textView.setVisibility(View.GONE);
+
+                } else {
+//                    On failure, display - SORRY, FAILED TO FETCH DATA.
+                    textView.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        viewModel.makeApiCall();
     }
 }
