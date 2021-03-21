578aadec9e5413761042b16a32e6620748e224e1
Samuel Okwalinga <sam.ictdev@gmail.com>
2021-03-21 08:22:59 +0300

    Create APIClient Interface

--- app/src/main/java/com/samo/retrofitsamuel/network/APIClient.java
+++ app/src/main/java/com/samo/retrofitsamuel/network/APIClient.java
@@ -1,4 +1,14 @@
 package com.samo.retrofitsamuel.network;
 
+import com.samo.retrofitsamuel.viewmodel.PostModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface APIClient {
+
+//    Implenting the GET method so as to return the list of posts from the API.
+//Posts are generated from the JSONPlaceholder API where the BASE_URL is declared in the Retrofitinstance class.
+    @GET("posts")
+    Call<List<PostModel>> getPostList();
 }
